8520d9434baa1246817a7965b94db25322374615
Yaroslav Koniukhov <yarko19@icloud.com>
2020-04-12 20:59:14 +0300

    Add class CoffeeMachine

---
+++ Coffee machine/CoffeeMachine.swift
@@ -0,0 +1,23 @@
+//
+//  CoffeeMachine.swift
+//  Coffee machine
+//
+//  Created by Yaroslav Koniukvoh on 12.04.2020.
+//  Copyright © 2020 Yaroslav Koniukvoh. All rights reserved.
+//
+
+import Foundation
+
+class CoffeeMachine {
+    var water: Int = 12
+    var beans: Int = 8
+    var milk: Int = 6
+    
+    init(water: Int,
+         beans: Int,
+         milk: Int) {
+        self.water = water
+        self.beans = beans
+        self.milk = milk
+    }
+}
