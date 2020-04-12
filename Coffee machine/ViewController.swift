66c363919a67749260ed0cce664a6b1a72b89b45
Yaroslav Koniukhov <yarko19@icloud.com>
2020-04-12 20:29:30 +0300

    Initial Commit

---
+++ Coffee machine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coffee machine
+//
+//  Created by Yaroslav Koniukvoh on 12.04.2020.
+//  Copyright © 2020 Yaroslav Koniukvoh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
